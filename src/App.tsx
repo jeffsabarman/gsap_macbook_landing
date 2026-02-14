3c7140d5231e312b03076cf0e30f6db70cad0dba
Jeffrey Triandi <jeffsabarman@gmail.com>
2026-02-14 20:25:05 +0000

    Implement hero section

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import NavBar from "./components/NavBar.tsx";
+import Hero from "./components/Hero.tsx";
 
 
 const App = () => {
     return (
         <main>
             <NavBar />
+            <Hero />
         </main>
     )
 }
